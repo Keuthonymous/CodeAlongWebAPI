8a261748bd20567e2447aa1e4252b635b1a00898
Mike Daughtrey <mikie.daughtrey@gmail.com>
2017-06-01 09:46:18 +0200

    Messed with API config

--- WebAPICodeAlong/App_Start/WebApiConfig.cs
+++ WebAPICodeAlong/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace WebAPICodeAlong
 {
@@ -14,9 +16,11 @@ namespace WebAPICodeAlong
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
