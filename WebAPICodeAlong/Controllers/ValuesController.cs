9839138a06e0d0e4a26a0798cf8e809f72415b24
Mike Daughtrey <mikie.daughtrey@gmail.com>
2017-06-01 10:04:24 +0200

    Added Book model, and messed with ValuesController

--- WebAPICodeAlong/Controllers/ValuesController.cs
+++ WebAPICodeAlong/Controllers/ValuesController.cs
@@ -4,15 +4,55 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPICodeAlong.Models;
 
 namespace WebAPICodeAlong.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<Book> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Book> b = new List<Book>() {
+                new Book
+            {
+
+                ISBN = 1,
+                Title = "SomeThing",
+                Author = "Someone",
+                Description = "A book",
+                PublishedYear = 0001
+            },
+
+                new Book
+            {
+
+                ISBN = 2,
+                Title = "SomeThing Else",
+                Author = "Someone Else",
+                Description = "Another book",
+                PublishedYear = 0001
+            },
+                new Book
+            {
+
+                ISBN = 3,
+                Title = "Another thing",
+                Author = "Someone Different",
+                Description = "That persons book",
+                PublishedYear = 0001
+            },
+                new Book
+            {
+
+                ISBN = 4,
+                Title = "The Bible",
+                Author = "Someone imaginary",
+                Description = "Fantasy",
+                PublishedYear = 0001
+            }};
+
+            return b;
         }
 
         // GET api/values/5
